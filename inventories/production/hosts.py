104193af44d7a675a556414710b64eb7af636e4f
mi2428 <mi2428782020@gmail.com>
2021-08-11 17:28:44 +0900

    Add dynamic inventory script
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

---
+++ inventories/production/hosts.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+HOSTS_PATH = os.path.join(os.path.dirname(__file__), "edges")
+
+
+def main():
+  target_ips = []
+  with open(HOSTS_PATH) as fd:
+    for line in fd.read().split("\n"):
+      if not line or line[0] == "#":
+        continue
+      host, ip = line.split()
+      target_ips.append(ip)
+
+  print(json.dumps({
+    "all": target_ips
+  }))
+
+
+if __name__ == "__main__":
+  main()
